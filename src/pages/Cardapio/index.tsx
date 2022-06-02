8b02e79b2527b558d86b2b6760d9df68d869e286
SabrinaTavera <sabrina.tavera@hotmail.com>
2022-06-01 23:40:48 -0300

    Add layout input buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,10 @@
 import styles from "./Cardapio.module.scss"
 import { ReactComponent as  Logo } from "assets/logo.svg"
+import Buscador from "./Buscador"
+import { useState } from "react"
 
 export default function Cardapio(){
+    const[busca, setBusca] = useState("");
     return (
         <main>
             <nav className={styles.menu}>
@@ -14,6 +17,11 @@ export default function Cardapio(){
                 </div>
 
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+
+            </section>
         </main>
     )
 } 
